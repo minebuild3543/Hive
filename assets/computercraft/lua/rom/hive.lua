e2e8b69c196c5058fb55c2839a3603154de764a6
lupus <lupus590@users.noreply.github.com>
2015-03-05 17:01:27 +0000

    marked in compatibility check as to do

--- assets/computercraft/lua/rom/hive.lua
+++ assets/computercraft/lua/rom/hive.lua
@@ -1,2 +1,8 @@
 --this is the main program, this is what users will run
 --what this program actually does is update and bootstrap the actual program
+
+--check compatibility
+bunch of if statments checking for APIs
+	if can't load the api
+		if can't download the api
+			error? prompt to continue?
