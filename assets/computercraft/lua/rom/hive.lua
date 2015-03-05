a42f156b901ef5c8564cc4486782cfaf6a268ab7
lupus <lupus590@users.noreply.github.com>
2015-03-05 18:55:50 +0000

    added more to do

--- assets/computercraft/lua/rom/hive.lua
+++ assets/computercraft/lua/rom/hive.lua
@@ -2,7 +2,12 @@
 --what this program actually does is update and bootstrap the actual program
 
 --check compatibility
+--[[
 bunch of if statments checking for APIs
 	if can't load the api
 		if can't download the api
 			error? prompt to continue?
+			
+--launch the program
+
+--error recovery 
