2e663d22d77a37227284dcf54c5025eb7e936c75
lupus <lupus590@users.noreply.github.com>
2015-03-05 18:56:34 +0000

    changed path variable

--- assets/computercraft/lua/rom/Hive/autorun.lua
+++ assets/computercraft/lua/rom/Hive/autorun.lua
@@ -2,7 +2,7 @@
 
 --this file calls components of hive that should be run from startup every time
 --as the computers startup walks through each file running them one after the other returns, we can combine them as one file (thus, this)
-path = "../components/"
+local path = "/components/"
 shell.run(path.."AutoLabel.lua")
 if(turtle) then
 	--shell.run(path.."AutoFuel.lua")--better to put this into the turtle api
