216830180c0a3ab75c259db161490c25107940e5
lupus <lupus590@users.noreply.github.com>
2015-03-31 23:31:54 +0100

    more stuff

---
+++ src/downloader.lua
@@ -0,0 +1,9 @@
+--this file will eventually be ported to pastebin
+--its purpose is to download the github repo to the CC computer
+
+--TODO: download files
+
+--TODO: ask if hive should run on startup
+--if the computer is a turtle then 5 seconds to auto yes (with option to say no), this is to allow for turtle factories
+--if turle then this installer will ask for a default nest (hive server)
+--
--- src/hive.lua
+++ src/hive.lua
@@ -10,11 +10,22 @@ bunch of if statments checking for APIs
 		if can't download the api
 			error? prompt to continue?
 ]]
+
+shell.run("AutoLabel.lua")
+
 --set aliases for everything (remove the .lua extension and need for file path)	
-shell.setAlias(string alias, string program) 
+--shell.setAlias(string alias, string program)
 
---launch the program
+--shell.setAlias() 
 
---error recovery 
+--TODO:launch the program
+	--nest(server)
+		--task master
+		--drone tracker
+	--drone(client)
+		--tracking device
+		--task executer
+
+--TODO:error recovery 
 
 
